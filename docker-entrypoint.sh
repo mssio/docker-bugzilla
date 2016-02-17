08b982c702919cb6f51cc7133d36a95ccd5f7e7d
Mario Suharja <mario.stefano@ions-tech.com>
2016-02-17 16:38:36 +0700

    Optimize Bugzilla
    Use volume in Bugzilla so it will works on exisitng Bugzilla installation.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-/bin/sed -i "s/TTBZ_DB_ROOT_PASS/${MYSQL_ENV_MYSQL_ROOT_PASSWORD}/" /var/www/bugzilla/localconfig
-/bin/sed -i "s/TTBZ_APP_SECRET_KEY/${TTBZ_APP_SECRET_KEY}/" /var/www/bugzilla/localconfig
-echo "${TTBZ_APP_ADMIN_EMAIL}
-${TTBZ_APP_ADMIN_REAL_NAME}
-${TTBZ_APP_ADMIN_PASSWORD}
-${TTBZ_APP_ADMIN_PASSWORD}" > /tmp/answer.txt
-/var/www/bugzilla/checksetup.pl < /tmp/answer.txt
-rm /tmp/answer.txt
+cd /var/www/bugzilla
 
-/bin/sed -i "s/TTBZ_APP_SERVER_NAME/${TTBZ_APP_SERVER_NAME}/" /etc/apache2/sites-available/001-bugzilla.conf
-/bin/sed -i "s/TTBZ_APP_SERVER_ADMIN/${TTBZ_APP_SERVER_ADMIN}/" /etc/apache2/sites-available/001-bugzilla.conf
+if ! [ -e /var/www/bugzilla/index.cgi ]; then
+  echo "Bugzilla not exist copy it from source"
+  tar cf - --one-file-system -C /usr/src/bugzilla . | tar xf -
+else
+  echo "Bugzilla exist, skip copy from source"
+fi
+
+/bin/cp /usr/src/bugzilla/localconfig /var/www/bugzilla/localconfig
+
+/usr/bin/perl /var/www/bugzilla/install-module.pl Email::Send
+/usr/bin/perl /var/www/bugzilla/install-module.pl --all
+
+/bin/sed -i "s/MSSBZ_APP_DB_NAME/${MSSBZ_APP_DB_NAME}/" /var/www/bugzilla/localconfig
+/bin/sed -i "s/MSSBZ_APP_DB_USER/${MSSBZ_APP_DB_USER}/" /var/www/bugzilla/localconfig
+/bin/sed -i "s/MSSBZ_APP_DB_PASS/${MSSBZ_APP_DB_PASS}/" /var/www/bugzilla/localconfig
+/bin/sed -i "s/MSSBZ_APP_SECRET_KEY/${MSSBZ_APP_SECRET_KEY}/" /var/www/bugzilla/localconfig
+
+echo "${MSSBZ_APP_ADMIN_EMAIL}
+${MSSBZ_APP_ADMIN_REAL_NAME}
+${MSSBZ_APP_ADMIN_PASSWORD}
+${MSSBZ_APP_ADMIN_PASSWORD}" > /tmp/answer.txt
+/usr/bin/perl /var/www/bugzilla/checksetup.pl < /tmp/answer.txt
+/bin/rm /tmp/answer.txt
+
+/bin/sed -i "s/MSSBZ_APP_SERVER_NAME/${MSSBZ_APP_SERVER_NAME}/" /etc/apache2/sites-available/001-bugzilla.conf
 
 /usr/sbin/apache2ctl -D FOREGROUND
